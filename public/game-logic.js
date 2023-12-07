8f3d3fd7f5e85ee3ce92a4d6b7a3c5707dbbf3aa
Hortensjaa <lilka305@gmail.com>
2023-12-07 19:48:49 +0100

    working logic of checkers

--- public/game-logic.js
+++ public/game-logic.js
@@ -10,10 +10,10 @@ class Game {
             [0, 2, 0, 2, 0, 2, 0, 2],   // y = 0
             [2, 0, 2, 0, 2, 0, 2, 0],   // y = 1
             [0, 2, 0, 2, 0, 2, 0, 2],   // y = 2
-            [0, 0, 0, 0, 0, 0, 0, 0],   // y = 3
+            [0, 0, 1, 0, 0, 0, 0, 0],   // y = 3
             [0, 0, 0, 0, 0, 0, 0, 0],   // y = 4
             [1, 0, 1, 0, 1, 0, 1, 0],   // y = 5
-            [0, 1, 0, 1, 0, 1, 0, 1],   // y = 6
+            [0, 1, 0, 0, 0, 1, 0, 1],   // y = 6
             [1, 0, 1, 0, 1, 0, 1, 0]    // y = 7
         ]
         this.white_checkers_num = 12
@@ -50,15 +50,22 @@ class Game {
     // checking capture obligation - returns list of (possibly) multiple jumps
     check_capture_obligation(x, y) {
         let res = [];
+        let yis;
+        if (this.is_king(x, y)) {
+            yis = [-1, 1]
+        } else {
+            yis = this.white_turn ? [-1] : [1]
+        }
         ([-1, 1]).forEach(xi => {
-            let yi = this.white_turn ? -1 : 1
-            if (this.in_board_bounds(x + xi, y + yi) && this.in_board_bounds(x + 2 * xi, y + 2 * yi)) {
-                if (this.is_opposite_color(x + xi, y + yi) && this.is_empty(x + 2 * xi, y + 2 * yi)) {
-                    let new_arr = this.check_capture_obligation(x + 2 * xi, y + 2 * yi);
-                    new_arr.unshift([x + 2 * xi, y + 2 * yi]);
-                    res = res.concat(new_arr);
+            (yis).forEach(yi => {
+                if (this.in_board_bounds(x + xi, y + yi) && this.in_board_bounds(x + 2 * xi, y + 2 * yi)) {
+                    if (this.is_opposite_color(x + xi, y + yi) && this.is_empty(x + 2 * xi, y + 2 * yi)) {
+                        let new_arr = this.check_capture_obligation(x + 2 * xi, y + 2 * yi);
+                        new_arr.unshift([x + 2 * xi, y + 2 * yi]);
+                        res = res.concat(new_arr);
+                    }
                 }
-            }
+            })
         });
         return res;
     }
@@ -86,11 +93,18 @@ class Game {
     // ---------------- getting list of possible moves without capture ----------------
     get_possible_moves(x, y) {
         let res = [];
+        let yis;
+        if (this.is_king(x, y)) {
+            yis = [-1, 1]
+        } else {
+            yis = this.white_turn ? [-1] : [1]
+        }
         ([-1, 1]).forEach(xi => {
-            let yi = this.white_turn ? -1 : 1
-            if (this.in_board_bounds(x + xi, y + yi) && this.is_empty(x + xi, y + yi)) {
-                res.push([x + xi, y + yi])
-            }
+            (yis).forEach(yi => {
+                if (this.in_board_bounds(x + xi, y + yi) && this.is_empty(x + xi, y + yi)) {
+                    res.push([x + xi, y + yi])
+                }
+            })
         });
         return {
             checker: [x, y],
@@ -178,6 +192,8 @@ class Game {
             this.board[(old_y + new_y) / 2][(old_x + new_x) / 2] = 0
             this.white_turn ? this.black_checkers_num-- : this.white_checkers_num--
         }
+
+        console.log(`moved from ${JSON.stringify([old_x, old_y])} to ${JSON.stringify([new_x, new_y])}`)
     }
 
     // ---------------- game ending ----------------
@@ -200,21 +216,45 @@ class Game {
         try {
 
             // moves - list of possible moves in current turn for active player
-            let moves = this.check_capture_obligation_for_all()
-            if (moves.length === 0) {
-                moves = this.get_possible_moves_for_all()
+            let possible_moves = this.check_capture_obligation_for_all()
+            if (possible_moves.length === 0) {
+                possible_moves = this.get_possible_moves_for_all()
                 with_capture = false
             }
-            this.print_available_moves(moves)
+            this.print_available_moves(possible_moves)
             let coords = this.get_input();
 
             // check if chosen move is possible
-            while (!this.move_validation(coords, moves)) {
+            while (!this.move_validation(coords, possible_moves)) {
                 coords = this.get_input();
             }
 
             // making a move
-            this.make_move(coords, with_capture)
+            if (!with_capture) {
+                this.make_move(coords, false)
+            } else {
+                // while capturing, we can make multiple moves in one turn
+                let old_x, old_y, new_x, new_y;
+                [old_x, old_y, new_x, new_y] = coords;
+                // possible moves for chosen checker
+                let moves_for_checker =
+                    possible_moves.filter(move => (move.checker[0] === old_x && move.checker[1] === old_y))[0].ends
+                // creating list of moves to be done (because only one capture is obligatory, not all possible)
+                let coords_id = -1;
+                for (let i = 0; i < moves_for_checker.length; i++) {
+                    if ((moves_for_checker[i][0] === new_x) && (moves_for_checker[i][1] === new_y)) {
+                        coords_id = i;
+                    }
+                }
+                let multiple_moves = moves_for_checker.slice(0, coords_id + 1);
+                // making moves from list
+                for (let move of multiple_moves) {
+                    this.make_move([old_x, old_y, move[0], move[1]], true)
+                    old_x = move[0];
+                    old_y = move[1];
+                }
+            }
+
             this.white_turn = !this.white_turn
         } catch (e) {
             console.error(e.message);
@@ -226,8 +266,8 @@ class Game {
     // whole game as a function
     async game_loop() {
         while (!this.is_game_over()) {
-            console.table(this.board);
             await this.turn.call(this);
+            console.table(this.board);
         }
         return this.get_winner();
     }
