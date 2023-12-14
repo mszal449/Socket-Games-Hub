3712c7d5d64a94c96c260ec137a05c68036288dc
mszal449 <m.szalasz@gmail.com>
2023-12-14 12:26:57 +0100

    Switched from token authorization to signed cookie system.

--- middleware/authentication.js
+++ middleware/authentication.js
@@ -3,29 +3,15 @@ import jwt from 'jsonwebtoken'
 
 // Authentication middleware
 const auth = async (req, res, next) => {
-    try {
-        // Read authentication token
-        const token = await req.cookies.token
-
-        if (!token) {
-            // If token was not found, return the information to user
-            req.user = { authenticated: false }
-        } else {
-            // Verify the token
-            const payload = await jwt.verify(token, process.env.JWT_SECRET)
-
-            // Return data stored inside the token and confirm authentication
-            req.user = { userId: payload.userId, username: payload.username, authenticated: true }
-        }
-
+    console.log('inside middleware')
+    if (req.signedCookies.user) {
+        req.user = req.signedCookies.user
+        console.log('good cookie')
         next()
-    } catch (error) {
-        // In case of an error, end session
-        res.clearCookie('token')
-        req.user = { authenticated: false }
-        next()
+    } else {
+        res.clearCookie('user')
+        res.redirect('/auth/login?returnUrl' + req.url)
     }
 }
-
 
 export default auth
