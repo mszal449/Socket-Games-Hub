d0d33b46ead68b620c8831309d8125d524dbdfc5
Hortensjaa <lilka305@gmail.com>
2024-01-17 08:52:59 +0100

    redirect from not existing room fix

--- socket/connection.js
+++ socket/connection.js
@@ -19,57 +19,57 @@ function handleConnection(io) {
                 // Create a new game instance for the room if it doesn't exist
                 games[room] = new Game()
             }
+
+            const game = games[room]
+
+            if (!([game.white_player, game.black_player].includes(username)) && !game.is_active()) {
+                // Entering a game
+                game.add_player(username)
+            }
+            if ([game.white_player, game.black_player].includes(username)) {
+                // Entering a game OR refreshing screen
+                io.to(room).emit('gameUpdate', game, room)
+                io.to(room).emit('playerEntered')
+            } else {
+                // Can't enter the game - two players are already in
+                socket.emit('cantEnter')
+                socket.leave(room)
+            }
+
+            socket.on('gameOver', () => {
+                let [winner, color] = game.get_winner()
+                io.to(room).emit('gameOver', [winner, color])
+                delete games[room]
+                socket.emit('deleteRoom', room)
+                socket.leave(room)
+            })
+
+            socket.on('selectChecker', (coords) => {
+                if (username === game.active_player()) {
+                    game.select_checker(coords)
+                    socket.emit('checkerSelected', coords)
+                }
+            })
+
+            socket.on('moveChecker', (path_and_coords) => {
+                if (username === game.active_player()) {
+                    game.validate_and_make_move(path_and_coords)
+                    io.to(room).emit("gameUpdate", game)
+                }
+            })
+
+            socket.on('disconnect', () => {
+                socket.broadcast.to(room).emit('opponentDisconnected')
+            })
+
+            socket.on('endGameWalkover', () => {
+                delete games[room]
+                socket.emit('deleteRoom', room)
+                socket.leave(room)
+            })
         } else {
             socket.emit('redirect', '/game/dashboard')
         }
-
-        const game = games[room]
-
-        if (!([game.white_player, game.black_player].includes(username)) && !game.is_active()) {
-            // Entering a game
-            game.add_player(username)
-        }
-        if ([game.white_player, game.black_player].includes(username)) {
-            // Entering a game OR refreshing screen
-            io.to(room).emit('gameUpdate', game, room)
-            io.to(room).emit('playerEntered')
-        } else {
-            // Can't enter the game - two players are already in
-            socket.emit('cantEnter')
-            socket.leave(room)
-        }
-
-        socket.on('gameOver', () => {
-            let [winner, color] = game.get_winner()
-            io.to(room).emit('gameOver', [winner, color])
-            delete games[room]
-            socket.emit('deleteRoom', room)
-            socket.leave(room)
-        })
-
-        socket.on('selectChecker', (coords) => {
-            if (username === game.active_player()) {
-                game.select_checker(coords)
-                socket.emit('checkerSelected', coords)
-            }
-        })
-
-        socket.on('moveChecker', (path_and_coords) => {
-            if (username === game.active_player()) {
-                game.validate_and_make_move(path_and_coords)
-                io.to(room).emit("gameUpdate", game)
-            }
-        })
-
-        socket.on('disconnect', () => {
-            socket.broadcast.to(room).emit('opponentDisconnected')
-        })
-
-        socket.on('endGameWalkover', () => {
-            delete games[room]
-            socket.emit('deleteRoom', room)
-            socket.leave(room)
-        })
     })
 }
 
